d033f9363de1071a9912fdd4a5d5b2c63038bccf
mleiva2 <manuel.leiva@pmi.com>
2022-12-12 11:42:01 +0100

    day4 1 gold star

--- day4/day4.py
+++ day4/day4.py
@@ -11,46 +11,29 @@ def main():
         print(sepbycoma)
         sepbydash1 = sepbycoma[0].split('-') #separate the 1st chunk by the dash
         sepbydash2 = sepbycoma[1].split('-') #separate the 2nd chunk by the dash
-        print(sepbydash1[0], sepbydash2[0]) #this is a and b 
-        print(sepbydash1[1], sepbydash2[1]) # this is c and d
-        
-        if sepbydash1[0] == sepbydash1[1] and sepbydash2[0] == sepbydash2[1]: # a = c, b = d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        #print(sepbydash1[0], sepbydash2[0]) #this is a and b 
+        #print(sepbydash1[1], sepbydash2[1]) # this is c and d
+        a = int(sepbydash1[0])
+        c = int(sepbydash1[1])
+        b = int(sepbydash2[0])
+        d = int(sepbydash2[1])
+        print(a, c, b, d)
+        if a == b and c == d: # a = c, b = d
+            print("hola")
             count = count+1
-        elif sepbydash1[0] > sepbydash2[1] and sepbydash2[0] < sepbydash2[1]: # a > c, b < d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        elif a > b and c < d: # a > c, b < d
             count = count+1
-        elif sepbydash1[0] > sepbydash2[1] and sepbydash2[0] == sepbydash2[1]: # a > c, b = d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        elif a > b and c == d: # a > c, b = d
             count = count+1
-        elif sepbydash1[0] < sepbydash2[1] and sepbydash2[0] > sepbydash2[1]: # a < c, b > d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        elif a < b and c > d: # a < c, b > d
             count = count+1
-        elif sepbydash1[0] < sepbydash2[1] and sepbydash2[0] == sepbydash2[1]: # a < c, b = d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        elif a < b and c == d: # a < c, b = d
             count = count+1
-        elif sepbydash1[0] == sepbydash2[1] and sepbydash2[0] > sepbydash2[1]: # a = c, b > d
-            print(sepbydash1[0])
-            print(sepbydash1[1])
-            print(sepbydash2[0])
-            print(sepbydash2[1])
+        elif a == b and c > d: # a = c, b > d
+            count = count+1
+        elif a == b and c < d: # a = c, b < d
             count = count+1
         else:
             print(":(")
-
+    print(count)
 main()
