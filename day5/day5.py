c5b3c0c37dcbe54a08e581ffc48dfcf42d0af02c
mleiva2 <manuel.leiva@pmi.com>
2022-12-19 09:57:37 +0100

    new update

--- day5/day5.py
+++ day5/day5.py
@@ -4,10 +4,23 @@ def readdata():
 
 def main():
     alldata = readdata()
-    print((alldata[0]))
-    print(alldata[1])
-    print(alldata[2])
-    x = alldata[3].split()
-    print(x[0])
-
+    longitud = len(alldata)
+    for i in range(0,longitud):
+        numero=0
+        splitthat = 2
+        d = alldata[i]
+        separation = d.split(d[1])
+        print("HOola",separation)
+        for i in d:#iterate through the full line to detect the letter
+            print(i)
+            # numero = numero+1
+            # if i == "[":
+            #     letra = d[numero]
+            #     print(letra)
+            # elif i == "1":
+            #     print("hola")
+    numbers = alldata[3].split()
+    print(numbers[0])
+    print(numbers[1])
+    print(numbers[2])
 main()
---
+++ day5/test.py
@@ -0,0 +1,3 @@
+cadena = "cadena"
+subcadenas = cadena.split(cadena[5], 3)
+print(subcadenas)
